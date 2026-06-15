1804fc5a9bb6397a5bb5683b87e1ad99ea1e0f5f
vinicius.A <vinicius.araruna18@gmail.com>
2026-06-14 23:08:46 -0300

    feat(kruskal): adiciona kruskal_construir_floresta com limiar de peso

--- include/kruskal.h
+++ include/kruskal.h
@@ -5,4 +5,9 @@
 
 Grafo *kruskal_construir_agm_maxima(const Grafo *grafo);
 
+/* Constroi uma floresta geradora maxima ignorando arestas com
+ * peso < limiar_peso. Vertices sem arestas validas ficam isolados.
+ * Retorna NULL em caso de erro. */
+Grafo *kruskal_construir_floresta(const Grafo *grafo, int limiar_peso);
+
 #endif
